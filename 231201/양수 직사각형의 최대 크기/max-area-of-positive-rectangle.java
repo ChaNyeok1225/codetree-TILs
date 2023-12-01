fe82e3d305f781b38c36f0688de38fd8a78e1e6c
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-12-01 12:09:27 +0900

    Title: 양수 직사각형의 최대 크기, Time: 105ms, Memory: 9MB, Status: Passed - CodeTree

--- 231201/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
+++ 231201/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -16,66 +16,72 @@ public class Main {
 		row = Integer.parseInt(st.nextToken());
 		col = Integer.parseInt(st.nextToken());
 		arr = new int[row][col];
-		
+
 		for (int i = 0; i < row; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < col; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
-				if (arr[i][j] < 1) continue;
+				if (arr[i][j] < 1)
+					continue;
 
 				k = 1;
-				width = 0;
 				while (j + k < col && arr[i][j + k] > 0) {
 					k++;
 				}
 				width = k;
-				for (int q = 1; q < width; q++) {
+				for (int q = 1; q <= width; q++) {
 					l = 1;
 					height = 0;
 					size = 0;
-					outer : while (i + l < row && arr[i + l][j] > 0) {
+					outerA: while (i + l < row && arr[i + l][j] > 0) {
 						for (int w = 1; w < q; w++) {
 							if (arr[i + l][j + w] < 1) {
-								break outer;
+								break outerA;
 							}
 						}
 						l++;
 					}
 					height = l;
 					size = q * height;
-					if (maxSize < size) maxSize = size;
+					if (maxSize < size)
+						maxSize = size;
 				}
-				
+
 				k = 1;
-				height = 0;
 				while (i + k < row && arr[i + k][j] > 0) {
 					k++;
 				}
 				height = k;
-				for (int q = 1; q < height; q++) {
+				for (int q = 1; q <= height; q++) {
 					l = 1;
 					width = 0;
 					size = 0;
-					outer : while (j + l < col && arr[i][j + l] > 0) {
+					outerB: while (j + l < col && arr[i][j + l] > 0) {
 						for (int h = 1; h < q; h++) {
 							if (arr[i + h][j + l] < 1) {
-								break outer;
+								break outerB;
 							}
 						}
 						l++;
 					}
 					width = l;
 					size = width * q;
-					if (maxSize < size) maxSize = size;
+					if (maxSize < size)
+						maxSize = size;
 				}
+
+//				System.out.println(i);
+//				System.out.println(j);
+//				System.out.println(size);
+//				System.out.println("=================");
 			}
 		}
 		System.out.println(maxSize > 0 ? maxSize : -1);
 	}
-	
+
 }
