e3e59d73c7b854507a7a7f822fc75033b2cfbb1a
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-12-01 11:52:33 +0900

    Title: 양수 직사각형의 최대 크기, Time: 114ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 231201/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
+++ 231201/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -26,40 +26,50 @@ public class Main {
 		
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
+				if (arr[i][j] < 1) continue;
+
 				int k = 1;
 				int l = 1;
 				int width = 0;
-				int height = 1;
+				int height = 0;
 				int size = 0;
-				
-				if (arr[i][j] < 1) continue;
-				
-//				크기 체크 추가
 				while (j + k < col && arr[i][j + k] > 0) {
 					k++;
 				}
 				width = k;
-				
 				outer : while (i + l < row && arr[i + l][j] > 0) {
-					boolean flag = true;
 					for (int w = 1; w < width; w++) {
 						if (arr[i + l][j + w] < 1) {
-							flag = false;
 							break outer;
 						}
 					}
 					l++;
 				}
 				height = l;
-				
 				size = width * height;
 				if (maxSize < size) maxSize = size;
-//				if (size > 4) {
-//					System.out.println(i);
-//					System.out.println(j);
-//					System.out.println(size);
-//					System.out.println("==================");
-//				}
+				
+				k = 1;
+				l = 1;
+				width = 0;
+				height = 0;
+				size = 0;
+				while (i + k < row && arr[i + k][j] > 0) {
+					k++;
+				}
+				height = k;
+				outer : while (j + l < col && arr[i][j + l] > 0) {
+					for (int h = 1; h < height; h++) {
+						if (arr[i + h][j + l] < 1) {
+							break outer;
+						}
+					}
+					l++;
+				}
+				width = l;
+				size = width * height;
+				
+				if (maxSize < size) maxSize = size;
 			}
 		}
 		System.out.println(maxSize > 0 ? maxSize : -1);
