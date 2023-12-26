844567fb2f474def24d8e090dab0b14b8156a622
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-12-26 23:17:29 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 846ms, Memory: 31MB, Status: Passed - CodeTree

--- 231226/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-don't-overlap.java
+++ 231226/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-don't-overlap.java
@@ -12,23 +12,43 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        long[][] lines = new long[m][2];
+        long[][] line = new long[m][2];
+
         for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            lines[i][0] = Long.parseLong(st.nextToken());
-            lines[i][1] = Long.parseLong(st.nextToken());
+            line[i][0] = Long.parseLong(st.nextToken());
+            line[i][1] = Long.parseLong(st.nextToken());
         }
-        Arrays.sort(lines, (a,b) -> Long.compare(a[0],b[0]));
+        Arrays.sort(line, (a,b) -> Long.compare(a[0],b[0]));
 
-        long start = 1, end = lines[m-1][1] - lines[0][0], mid;
+
+        long start = 1, end = line[m-1][1] - line[0][0];
+        long mid;
         long ans = 0;
 
         while(start <= end) {
             mid = start + (end - start) / 2;
 
+            long point = Long.MIN_VALUE;
             long cnt = 0;
-            for(int i = 0; i < m; i++) 
-                cnt += (( lines[i][1] - lines[i][0] ) / mid ) + 1;
+
+            for(int i = 0; i < m; i++) {
+                if(point + mid > line[i][1])
+                    continue;
+                
+                if(point + mid > line[i][0]) {
+                    point += mid;
+                } else
+                    point = line[i][0];
+                cnt++;
+                
+                long dis = line[i][1] - point;
+                long val = dis / mid;
+                cnt += val;
+
+                point += val * mid;
+            }
+
             if(cnt >= n) {
                 start = mid + 1;
                 ans = mid;
@@ -37,6 +57,7 @@ public class Main {
 
         }
         
+
         System.out.println(ans);
         
     }
