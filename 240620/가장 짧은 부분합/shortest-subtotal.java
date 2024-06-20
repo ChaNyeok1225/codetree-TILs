3d942dad3666c79d6e18933ed9f95e51b4fa6efd
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-06-20 15:43:12 +0900

    Title: 가장 짧은 부분합, Time: 250ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240620/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int answer = Integer.MAX_VALUE;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int e = 0, sum = 0;
+        for(int i = 0; i < n; i++) {
+            while(e < n && sum < s) {
+                sum += arr[e++];
+            }
+            if(sum >= s) {
+                if(e - i < answer)
+                    answer = e - i;
+            }
+            sum -= arr[i];
+        }
+        System.out.println(answer);
+    }
+}
