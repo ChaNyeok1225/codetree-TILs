532226afa1bc9974a25a2840a59b64e803bfb68e
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-07-16 15:37:06 +0900

    Title: 세 수의 합, Time: 309ms, Memory: 22MB, Status: Passed - CodeTree

--- 240716/세 수의 합/sum-of-three-num.java
+++ 240716/세 수의 합/sum-of-three-num.java
@@ -1,48 +1,49 @@
-import java.util.*;
-import java.io.*;
-public class Main {
-    static int n;
-    static int k;
-    static int [] arr;
-    static StringTokenizer stk;
-    public static void main(String[] args) throws Exception{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        stk = new StringTokenizer(bf.readLine());
-        n = Integer.parseInt(stk.nextToken());
-        k = Integer.parseInt(stk.nextToken());
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        stk = new StringTokenizer(bf.readLine());
-        arr = new int[n];
-        for(int i= 0;i<n;i++){
-            arr[i] = Integer.parseInt(stk.nextToken());
-            if(map.containsKey(arr[i])){
-                map.put(arr[i], map.get(arr[i])+1);
-            }else{
-                map.put(arr[i], 1);
-            }
-        }
-        int cnt = 0;
-        // 특정 기준점 이후를 기준으로 계산하려고 하면 중복계산이 많다.
-        // 그래서 i까지의 계산만 유도한다면, 중복연산에 대한 복잡성이 줄어든다.
+import java.util.Scanner;
+import java.util.HashMap;
 
-        // 1 2 1 4 -1
-        for(int i= 0;i<n;i++){
-            // 지금 사용할 점에 대해서 사용했다고 표시를 남긴다.
-            // 그리고 세 수가 j < i < diff 인 값을 찾는다. 
-            // 자연스럽게 중복이 제거되는 이유는 기준점 i를 기준으로 그 앞에 원소들은 전부 제거되기 때문
-            if(map.containsKey(arr[i])){
-                map.put(arr[i], map.get(arr[i])-1);
-            }
-            for(int j= 0;j<i;j++){
-                int diff = k - arr[i] - arr[j];
-                if(map.containsKey(diff)){
-                    cnt += map.get(diff);
-                }
-            }
-            
-        }
-        System.out.println(cnt);
-    }
+public class Main {
+    public static final int MAX_N = 1000;
     
+    // 변수 선언
+    public static int n, k;
+    public static int[] arr = new int[MAX_N];
+    public static HashMap<Integer, Integer> freq = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        int ans = 0;
+
+        // 각 숫자가 몇 번씩 나왔는지를
+        // hashmap에 기록해줍니다.
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if(!freq.containsKey(arr[i]))
+                freq.put(arr[i], 1);
+            else
+                freq.put(arr[i], freq.get(arr[i]) + 1);
+        }
+
+        // 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+        for(int i = 0; i < n; i++) {
+            // 이미 순회한 적이 있는 숫자는 빼 버림으로서
+            // 같은 조합이 여러번 세어지는 걸 방지합니다.
+            if(!freq.containsKey(arr[i]))
+                freq.put(arr[i], -1);
+            else
+                freq.put(arr[i], freq.get(arr[i]) - 1);
+
+            for(int j = 0; j < i; j++) {
+                // 전처리를 해주었기 때문에 이미 순회한 적 있는 값은 hashmap에 없습니다.
+                // 이와 같은 방식으로 같은 조합이 중복되어 세어지는 걸 방지할 수 있습니다.
+                if(freq.containsKey(k - arr[i] - arr[j]))
+                    ans += freq.get(k - arr[i] - arr[j]);
+            }
+        }
+
+        System.out.print(ans);
+    }
 }
