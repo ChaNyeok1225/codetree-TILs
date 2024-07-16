539b5f4184a0861f9d631af9268e3a3a3c1cfafa
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-07-16 12:23:22 +0900

    Title: 두 수의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240716/두 수의 합/sum-of-two-num.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            map.put(arr[i], map.getOrDefault(arr[i],0) + 1);
+        }
+        Arrays.sort(arr);
+
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            map.put(arr[i], map.get(arr[i]) - 1);
+            ans += map.get(k - arr[i]);
+        }
+
+        System.out.println(ans);
+
+    }
+}
