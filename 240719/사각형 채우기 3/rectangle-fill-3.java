e8028b400a9d0dba9b6b7701db10705862579435
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-07-19 15:55:45 +0900

    Title: 사각형 채우기 3, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240719/사각형 채우기 3/rectangle-fill-3.java
+++ 240719/사각형 채우기 3/rectangle-fill-3.java
@@ -8,11 +8,18 @@ public class Main {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+2];
+        long[] dp = new long[n+2];
+        dp[0] = 1;
         dp[1] = 2;
         dp[2] = 7;
-        for(int i = 4; i <= n; i++) {
-            dp[i] = (dp[i-1] * 2 + dp[i-2]) % 1_000_000_007;
+
+        long sum = 0;
+        long acc = 0;
+        for(int i = 3; i <= n; i++) {
+            
+            sum = (dp[i-1] * 2 + dp[i-2] * 3) % 1_000_000_007;
+            acc = (acc + dp[i-3] * 2) % 1_000_000_007;
+            dp[i] = (sum + acc) % 1_000_000_007;
         }
 
         System.out.println(dp[n]);
