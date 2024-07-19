3f0e5d33dea0f8defc5a866d78d612f842edbebb
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-07-19 15:05:05 +0900

    Title: 계단 오르기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240719/계단 오르기/climbing-stairs.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+3];
+        dp[0] = dp[2] = 1;
+        for(int i = 3; i <= n; i++) {
+            dp[3] = dp[i-2] + dp[i-3];
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
