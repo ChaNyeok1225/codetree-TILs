2e058f3fe35957154395248e2b764d92e0274b74
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-07-19 13:26:15 +0900

    Title: 피보나치 수, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

--- 240719/피보나치 수/fibonacci-number.java
+++ 240719/피보나치 수/fibonacci-number.java
@@ -11,7 +11,7 @@ public class Main {
 
         int[] arr = new int[n + 2];
         arr[1] = arr[2] = 1; 
-        for(int i = 3; i < n; i++) {
+        for(int i = 3; i <= n; i++) {
             arr[i] = arr[i-1] + arr[i-2];
         }
 
