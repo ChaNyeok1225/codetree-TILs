fc530ccedf27e06ca9ea8d981e4a27a377b957de
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-07-29 22:14:15 +0900

    Title: 양수 직사각형의 최대 크기, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

--- 240729/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
+++ 240729/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -1,63 +1,56 @@
+//n과 m
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
-    static int n, m, ans;
-    static int[][] arr;
+    static int ans= -1;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+        StringTokenizer st= new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n= Integer.parseInt(st.nextToken());
+        int m= Integer.parseInt(st.nextToken());
 
-        ans = -1;
-        arr = new int[n][m];
+        int[][] map= new int[n][m];
 
-        for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<n; i++){
+            st= new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++){
+                map[i][j]= Integer.parseInt(st.nextToken());
             }
         }
 
+        int ans = -1;
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                down(i, j, 0, Integer.MAX_VALUE);
+
+                int h = 0;
+                int minW = Integer.MAX_VALUE;
+                for(int k = i; k < n; k++) {
+                    if(map[k][j] <= 0)
+                        break;
+                    h++;
+                    int w = 0;
+                    for(int l = j; l < m; l++) {
+                        if(map[k][l] <= 0)
+                            break;
+                        w++;
+                    }
+
+                    minW = minW < w ? minW : w;
+                    int area = h * minW;
+                    ans = ans > area ? ans : area;
+                }
             }
         }
 
-        System.out.println(ans);
+        System.out.print(ans);
 
     }
 
-    static void down(int r, int c, int h, int minW) {
-        if(valid(r, c))
-            return;
-        h++;
-
-        int w = 0;
-        for(int i = c; i < m; i++) {
-            if(arr[r][i] > 0)
-                w++;
-            else   
-                break;
-        }
-
-        minW = minW < w ? minW : w;
-        int area = h * minW; 
-        ans = ans > area ? ans : area;
-
-        down(r+1, c, h, minW);
-    }
-
-    static boolean valid(int r, int c) {
-        if(r >= n || c >= m || arr[r][c] <= 0)
-            return true;
-        return false;
-    }
+    
 }
