810dd6fd92680b0a66501d955b7d7a5ace301ca2
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-07-30 12:03:16 +0900

    Title: 최대 증가 부분 수열, Time: 126ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240730/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = 0;
+        int[] dp = new int[n];
+        for(int i = 0; i < n; i++) {
+            dp[i] = 1;
+
+            for(int j = 0; j < i; j++) {
+                if(arr[i] > arr[j]) {
+                    dp[i] = dp[i] > dp[j] + 1 ? dp[i] : dp[j] + 1;
+                }
+            }
+
+            ans = ans > dp[i] ? ans : dp[i];
+        }
+
+        System.out.println(ans);
+    }
+}
