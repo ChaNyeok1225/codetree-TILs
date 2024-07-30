9cfc6a3e2946231620a4c9f1a75e5664d8d7acc1
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-07-31 08:52:41 +0900

    Title: 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240731/동전 거슬러주기/coin-change.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        int[] dp = new int[m + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(st.nextToken());
+            dp[coins[i]] = 1;
+        }
+        
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = dp[i] < dp[i-coins[j]] + 1 ? dp[i] : dp[i-coins[j]] + 1 ;
+                }
+            }
+        }
+        System.out.println(dp[m]);
+
+    }
+}
