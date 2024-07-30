b1943af9d9001ac5b1a4418921900c905eab3a20
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-07-30 11:45:02 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 326ms, Memory: 20MB, Status: Passed - CodeTree

--- 240730/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
+++ 240730/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -10,6 +10,7 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
     
         int[][] arr = new int[n][n];
+        boolean[][] vis = new boolean[n][n];
         int min = Integer.MAX_VALUE, max = 0;
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -22,39 +23,41 @@ public class Main {
 
         int l, r, mid, ans = Integer.MAX_VALUE, nr, nc;
         int[] e, dr = {1,0}, dc = {0,1};
-        boolean pass;
         ArrayDeque<int[]> q = new ArrayDeque<>();
 
         for(int i = min; i <= max; i++) {
             l = i;
             r = max;
+
             while(l <= r) {
                 mid = l + (r - l) / 2;
-                pass = false;
+
+                for(int j = 0; j < n; j++) {
+                    Arrays.fill(vis[j], false);
+                }
 
                 if(arr[0][0] >= i && arr[0][0] <= (i+mid)) {
                     q.offer(new int[] {0, 0});
+                    vis[0][0] = true;
                 }
 
                 while(!q.isEmpty()) {
                     e = q.poll();
-                    if(e[0] == n - 1 && e[1] == n - 1) {
-                        pass = true;
-                    }
                     
                     for(int dir = 0; dir < 2; dir++) {
                         nr = e[0] + dr[dir];
                         nc = e[1] + dc[dir];
 
-                        if(nr >= n || nc >= n || arr[nr][nc] > (i+mid) || arr[nr][nc] < i) {
+                        if(nr >= n || nc >= n || vis[nr][nc] || arr[nr][nc] > (i+mid) || arr[nr][nc] < i) {
                             continue;
                         }
 
                         q.offer(new int[] {nr, nc});
+                        vis[nr][nc] = true;
                     }
                 }
 
-                if(pass) {
+                if(vis[n - 1][n - 1]) {
                     r = mid - 1;
                     ans = ans < mid ? ans : mid;
                 } else {
