3d07be1da38fb3153a0c2f0255f6c1ade7ff5dbf
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-08-05 09:36:10 +0900

    Title: 서로 다른 구간의 수, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240805/서로 다른 구간의 수/number-of-distinct-segments.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            map.put(s, map.getOrDefault(s, 0) + 1);
+            map.put(e + 1, map.getOrDefault(e + 1, 0) - 1);
+        }
+
+        int ans = 1;
+        int k = 0;
+        for(int val : map.values()) {
+            k += val;
+            if(k == 0) {
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
