503c7b20cede332d9de7ad17fb1fa17723a521c2
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-08-10 22:16:51 +0900

    Title: 정원 입장은 선착순, Time: 671ms, Memory: 28MB, Status: Passed - CodeTree

--- 240810/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.java
+++ 240810/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
@@ -11,19 +10,18 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         
-        int[][] arr = new int[n][];
+        int[][] arr = new int[n][3];
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            arr[i] = new int[] {i, a, t};
+            arr[i][0] = i;
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr, (a, b) -> a[1] - b[1]);
 
         int curIdx = 0;
         int curTime = arr[curIdx][1];
-        pq.offer(new int[] {arr[curIdx][0], arr[curIdx][1], arr[curIdx][2]});
+        pq.offer(arr[curIdx]);
         curIdx++;
 
         int ans = 0;
@@ -31,19 +29,13 @@ public class Main {
         while(!pq.isEmpty()) {
             cur = pq.poll();
 
-            if(cur != null) {
-                if(curTime < cur[1])
-                    curTime = cur[1];
-                ans = ans > curTime - cur[1] ? ans : curTime - cur[1];
-                curTime += cur[2];
-            }
+            if(curTime < cur[1])
+                curTime = cur[1];
+            ans = ans > curTime - cur[1] ? ans : curTime - cur[1];
+            curTime += cur[2];
 
-            while(curIdx < n && curTime >= arr[curIdx][1]) {
-                pq.offer(new int[] {arr[curIdx][0], arr[curIdx][1], arr[curIdx][2]});
-                curIdx++;
-            }
-            if(curIdx < n && pq.isEmpty()) {
-                pq.offer(new int[] {arr[curIdx][0], arr[curIdx][1], arr[curIdx][2]});
+            while(curIdx < n && (curTime >= arr[curIdx][1] || pq.isEmpty())) {
+                pq.offer(arr[curIdx]);
                 curIdx++;
             }
         }
