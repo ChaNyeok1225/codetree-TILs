df347d603e5c3b61704df6056f755790338b0b5d
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-09-20 14:11:59 +0900

    Title: 최대 이익 구하기 2, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240920/최대 이익 구하기 2/find-the-maximum-profit-2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] info = new int[n + 1][2];
+        for(int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            info[i][0] = Integer.parseInt(st.nextToken());
+            info[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n + 2];
+
+        for(int i = 1; i <= n; i++) {
+            dp[i] = Math.max(dp[i], dp[i-1]);
+
+            if(i + info[i][0] <= n + 1) {
+                dp[i+info[i][0]] = Math.max(dp[i+info[i][0]], dp[i] + info[i][1]);
+            }
+        }
+
+        System.out.println(dp[n+1]);
+    }
+}
