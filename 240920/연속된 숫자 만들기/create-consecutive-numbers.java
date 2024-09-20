e9e4cb5cdb4647c54011ec5fe897046b8701b833
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-09-20 14:57:49 +0900

    Title: 연속된 숫자 만들기, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240920/연속된 숫자 만들기/create-consecutive-numbers.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int[] point = new int[3];
+        point[0] = Integer.parseInt(st.nextToken());
+        point[1] = Integer.parseInt(st.nextToken());
+        point[2] = Integer.parseInt(st.nextToken());
+        Arrays.sort(point);
+
+        int diff1 = Math.abs(point[1]-point[0]);
+        int diff2 = Math.abs(point[1]-point[2]);
+
+        int dmax = Math.max(diff1,diff2);
+        int dmin = Math.min(diff1,diff2);
+
+        if(dmin > 2) {
+            sb.append("2");
+        } else if (diff1 == 1 && diff2 == 1){
+            sb.append("0");
+        } else {
+            sb.append("1");
+        } sb.append("\n");
+        sb.append(dmax - 1);
+
+        System.out.println(sb);
+
+    }
+}
