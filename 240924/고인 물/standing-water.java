afd18f936ea9b2bae6a6dd0f613eafc6ea8b6995
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-09-24 10:02:53 +0900

    Title: 고인 물, Time: 114ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240924/고인 물/standing-water.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int answer = 0;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        
+        int[] h = new int[m];
+        for(int i = 0; i < m; i++) 
+            h[i] = Integer.parseInt(st.nextToken());
+        
+        int l = 0, r = m - 1;
+        for(int i = 0; i < m; i++) {
+            if(h[l] <= h[i]) {
+                l = i;
+                continue;
+            }
+            answer += h[l] - h[i];
+        }
+
+        for(int i = m - 1; i >= l; i--) {
+            if(h[r] <= h[i]) {
+                r = i;
+                continue;
+            }
+            answer += h[r] - h[i];
+        }
+
+        System.out.println(answer);
+
+    }
+}
