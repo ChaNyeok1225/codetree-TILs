1bb5974f64f829f3fc2dc9ea59d3dc3a6a461c85
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-09-25 16:39:12 +0900

    Title: 알파벳 쟁탈전, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240925/알파벳 쟁탈전/alphabetics-competition.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static char[] sel;
+    static int[][] rules;
+    static int answer = 0;
+
+    public static void main(String[] args) throws Exception {
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        sel = new char[n];
+        rules = new int[k][3];
+
+        for(int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            char ins = st.nextToken().charAt(0);
+            rules[i][0] = ins == 'S' ? 0 : 1;
+            rules[i][1] = Integer.parseInt(st.nextToken());
+            rules[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0, n);
+
+        System.out.println(answer);
+    }
+
+    static void dfs(int idx, int size) {
+        if(idx == size) {
+
+            for(int i = 0; i < rules.length; i++) {
+                if(rules[i][0] == 0) {
+                    if(sel[rules[i][1]] != sel[rules[i][2]])
+                        return;
+                } else {
+                    if(sel[rules[i][1]] == sel[rules[i][2]])
+                        return;
+                }
+            }
+
+            answer++;
+            return;
+        }
+
+        for(int i = 0; i < 3; i++) {
+            sel[idx] = (char)(i + 'A');
+            dfs(idx+1, size);
+        }
+    }
+}
